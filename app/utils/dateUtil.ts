4e03533ebd2290919f1a551dc4c7cc1fa9bd3c5e
Maddox <jm@madev.me>
2020-05-15 14:13:57 -0500

    date parsing

--- app/utils/dateUtil.ts
+++ app/utils/dateUtil.ts
@@ -1,25 +1,26 @@
 import {getTime, format, parse} from 'date-fns'
 
-export function stringToDate(date : string) : Date {
-   let parsed = parse(date, 'yyyy-MM-dd', new Date());
-   if(parsed.toString() === "Invalid Date") {
-     parsed = parse(date, 'MM/dd/yyyy', new Date());
-   }
-   return parsed;
+export function stringToDate(date: string): Date {
+  let parsed = parse(date, 'yyyy-MM-dd', new Date());
+  if (parsed.toString() === "Invalid Date") {
+    parsed = parse(date, 'MM/dd/yyyy', new Date());
+  }
+  return parsed;
 }
 
-export function formatDate(time : number | string | Date) : string {
-  if(typeof time === 'string') {
+export function formatDate(time: number | string | Date): string {
+  if (typeof time === 'string') {
     time = timestamp(time);
   }
+  console.log('formatting', time);
   return format(
     time,
     'MM/dd/yyyy'
   )
 }
 
-export function timestamp(date : Date | number | string) {
-  if(typeof date === 'string') {
+export function timestamp(date: Date | number | string) {
+  if (typeof date === 'string') {
     date = stringToDate(date);
   }
   return getTime(date);
@@ -33,48 +34,98 @@ export function currentTimestamp() {
   return getTime(Date.now());
 }
 
-export function parseDate(value : string) {
+export function parseDate(value: string) {
+  try {
 
-  /*
-  // 5/5
+    if(!value.includes("/")) {
+      return parseDateNoSlashes(value);
+    }
+
+    // 5/5
+    const year = new Date().getFullYear();
+
+    if (value.length === 3) {
+      const month = parseInt(value[0]);
+      const date = parseInt(value[2]);
+      const parsed = stringToDate(`${year}-${month}-${date}`)
+      return formatDate(parsed);
+    }
+
+    // 05/05
+    if (value.length === 5 && (value[0] === '0' || value[3] === '0')) {
+      const month = `${value[0]}${value[1]}`;
+      const day = `${value[3]}${value[4]}`
+      return `${month}/${day}/${year}`;
+    }
+
+
+    if (value.length === 4 && value[1] === '/') {
+      const month = parseInt(`${value[0]}`);
+      const date = parseInt(`${value[2]}${value[3]}`);
+      const parsed = stringToDate(`${year}-${month}-${date}`)
+      return formatDate(parsed);
+    }
+
+//12/5
+    if (value.length === 4) {
+      const month = parseInt(`${value[0]}${value[1]}`);
+      const date = parseInt(value[3]);
+      console.log(`${year}-${month}-${date}`);
+      const parsed = stringToDate(`${year}-${month}-${date}`)
+      return formatDate(parsed);
+    }
+
+//12/12
+    if (value.length === 5) {
+      const month = parseInt(`${value[0]}${value[1]}`);
+      const date = parseInt(`${value[3]}${value[4]}`);
+      const parsed = stringToDate(`${year}-${month}-${date}`)
+      return formatDate(parsed);
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+
+  return value;
+}
+
+export function parseDateNoSlashes(value : string) {
+  const year = new Date().getFullYear();
+
+  // 22
+  if(value.length === 2) {
+    const month = value[0];
+    const date = value[1];
+    const parsed = stringToDate(`${year}-${month}-${date}`)
+    return formatDate(parsed);
+  }
+
+  // 312
+  // 330
+  // 103
   if(value.length === 3) {
-    const month = parseInt(value[0]);
-    const date = parseInt(value[2]);
-    const s = spacetime({month, date, year : currentDate().year()});
-    return formatDate(s, false);
+    let monthLength = 1;
+    if(parseInt(value[1]) === 0) {
+      monthLength = 2;
+    }
+    else if(parseInt(value[0] + value[1]) <= 12) {
+      monthLength = 2;
+    }
+    let month = monthLength === 1 ? value[0] : value[0] + value[1];
+    let date = monthLength === 1 ? value[1] + value[2] : value[2];
+    console.log(`${year}-${month}-${date}`)
+    const parsed = stringToDate(`${year}-${month}-${date}`)
+    return formatDate(parsed);
   }
 
-  // 05/05
-  if(value.length === 5 && (value[0] === '0' || value[3] === '0')) {
-    const month = `${value[0]}${value[1]}`;
-    const day = `${value[3]}${value[4]}`
-    const year = currentDate().year();
-    return `${month}/${day}/${year}`;
-  }
-
-  if(value.length === 4 && value[1] === '/') {
-    const month = parseInt(`${value[0]}`);
-    const date = parseInt(`${value[2]}${value[3]}`);
-    const s = spacetime({month, date, year : currentDate().year()});
-    return formatDate(s, false);
-  }
-
-  //12/5
+  // 1203
   if(value.length === 4) {
-    const month = parseInt(`${value[0]}${value[1]}`) + 1;
-    const date = parseInt(value[3]);
-    const s = spacetime({month, date, year : currentDate().year()});
-    return formatDate(s, false);
+    let month = value[0] + value[1];
+    let date = value[2] + value[3]
+    const parsed = stringToDate(`${year}-${month}-${date}`)
+    return formatDate(parsed);
   }
 
-  //12/12
-  if(value.length === 5) {
-    const month = parseInt(`${value[0]}${value[1]}`);
-    const date = parseInt(`${value[3]}${value[4]}`);
-    const s = spacetime({month, date, year : currentDate().year()});
-    return `${month == 12 ? s.month() + 1 : s.month()}/${s.date()}/${s.year()}`
-  }
-
-   */
   return value;
 }
